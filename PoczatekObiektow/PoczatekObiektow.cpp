12aa4aad238c7c4ea5e2ecb489a3f3415f762bc6
Karol Marszał <mkarolm3005@gmail.com>
2021-03-17 20:23:32 +0100

    Stworzenie klasy Trojkat i ustawienie dwoch metod

--- PoczatekObiektow/PoczatekObiektow.cpp
+++ PoczatekObiektow/PoczatekObiektow.cpp
@@ -1,20 +1,33 @@
-﻿// PoczatekObiektow.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    class Trojkat
+    {
+    private:
+        float a, h;
+    public:
+        void wczytajWartosci()
+        {
+            cout << "Podaj dlugosc podstawy trojkata: ";
+            cin >> a;
+            cout << "Podaj wysokosc trojkata: ";
+            cin >> h;
+        }
+        float zwrocPole()
+        {
+            return (a * h / 2);
+        }
+
+    };
+
+    Trojkat t1;
+    Trojkat* t2;
+    t1.wczytajWartosci();
+    cout << "Pole tego trojkata to: " << t1.zwrocPole() << endl;
+    t2 = new Trojkat();
+    t2->wczytajWartosci();
+    cout << "Pole drugiego trojkata to: " << t2->zwrocPole() << endl;
+    delete t2;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
