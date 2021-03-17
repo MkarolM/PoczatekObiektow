5a1eb81bfb301e9bc87c7d020c9e7d1d23b1e9e0
Karol Marszał <mkarolm3005@gmail.com>
2021-03-17 20:37:34 +0100

    Sprawdzenie czy da sie utorzyc trojkat

--- PoczatekObiektow/PoczatekObiektow.cpp
+++ PoczatekObiektow/PoczatekObiektow.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
+
     class Trojkat
     {
     private:
-        float a, h;
+        float a, h, b, c;
+        float t[3];
     public:
         void wczytajWartosci()
         {
@@ -19,15 +19,37 @@ int main()
         {
             return (a * h / 2);
         }
+        void stworzTrojkat()
+        {
+            cout << "Podaj 3 dlugosci bokow: ";
+            cin >> t[0] >> t[1] >> t[2];
+            if (t[0] > t[1]) swap(t[0], t[1]);
+            if (t[1] > t[2]) swap(t[1], t[2]);
+            if (t[0] > t[1]) swap(t[0], t[1]);
+            if (t[0] + t[1] > t[2])
+            {
+                cout << "Trojkat utworzony!";
+            }
+            else
+            {
+                cout << "Z takich dlugosci nie da sie utworzyc trojkata!" << endl;
+                stworzTrojkat();
+            }
+
+        }
 
     };
-
+int main()
+{
+        
     Trojkat t1;
     Trojkat* t2;
-    t1.wczytajWartosci();
-    cout << "Pole tego trojkata to: " << t1.zwrocPole() << endl;
+    //t1.wczytajWartosci();
+    //cout << "Pole tego trojkata to: " << t1.zwrocPole() << endl;
     t2 = new Trojkat();
-    t2->wczytajWartosci();
-    cout << "Pole drugiego trojkata to: " << t2->zwrocPole() << endl;
+    //t2->wczytajWartosci();
+    //cout << "Pole drugiego trojkata to: " << t2->zwrocPole() << endl;
     delete t2;
+    t2 = new Trojkat();
+    t2->stworzTrojkat();
 }
